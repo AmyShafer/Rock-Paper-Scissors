f92cf7e5e0dd09e6e4194db4d96ee424c2985b18
Amy Shafer <amy.u.shafer@gmail.com>
2021-12-19 23:14:06 -0500

    working on some bug in my code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,27 @@
+var rock = document.getElementById("rock");
+var paper = document.getElementById("paper");
+var scissors = document.getElementById("scissors");
 
 function rockPaperScissors(humanPick) {
   
-  var gameOutcome = humanPick + computerPick;
+  var gameOutcome = humanPick + computerPick(3);
   var humanWins = ["RS", "PR", "SP"];
   var computerWins = ["RP", "PS", "SR"]; 
-  var humanTally = 0;
-  var machineTally = 0;
+  //var humanTally = 0;
+  //var machineTally = 0;
 
   // These two variables are checking if the possible outcomes are in either the 
   // humanWins array or the computerWins array by using indexOf
   var didHumanWin = humanWins.indexOf(gameOutcome) > -1;
   var didComputerWin = computerWins.indexOf(gameOutcome) > -1;
 
-  if (humanPick === computerPick) {
-    // changeHumanGif
-    // changeMachineGif
-    // no change to score
-  } else if (didHumanWin) {
-    // changeHumanGif
-    // changeMachineGif
-    // add one to human score
-  } else if (didComputerWin) {
-    // changeHumanGif
-    // changeMachineGif
-    // add one to machine score
-  } else {
-    // changeHumanGif
-    // changeMachineGif
-    // no change to score
-  }
+  displayOutcome(gameOutcome, didHumanWin, didComputerWin);
 
-  return displayScoreboardUpdate(humanTally, machineTally);
+  console.log("Who wins? -- " + didHumanWin);
 }
 
 // Helper Function
-function computerPlay(num) {
+function computerPick(num) {
   var play = "X";
   var getNumber = Math.floor(Math.random() * num);
 
@@ -51,27 +38,31 @@ function computerPlay(num) {
   return play;
 }
 
-function displayScoreboardUpdate (addHuman, addMachine) {
-  
-}
 
-function displayMessage (outcome) {
-  if (humanPick === computerPick) {
-    document.getElementsByTagName("humanGifTie").style.display = flex;
-    document.getElementsByTagName("MachineGifTie").style.display = flex;
-  } else if (didHumanWin) {
-    document.getElementsByTagName("humanGifWin").style.display = flex;
-    document.getElementsByTagName("MachineGifLose").style.display = flex;
-  } else if (didComputerWin) {
-    document.getElementsByTagName("humanGifLose").style.display = flex;
-    document.getElementsByTagName("MachineGifWin").style.display = flex;
+function displayOutcome (outcome, humanWin, computerWin) {
+  
+  if (humanWin) {
+    document.getElementById("humanGifWin").style.display = "flex";
+    document.getElementById("humanGifWin").style.zIndex  = "1";
+    document.getElementById("machineGifLose").style.display = "flex";
+    document.getElementById("machineGifLose").style.zIndex  = "1";
+  } else if (computerWin) {
+    document.getElementById("humanGifLose").style.display = "flex";
+    document.getElementById("humanGifLose").style.zIndex  = "1";
+    document.getElementById("machineGifWin").style.display = "flex";
+    document.getElementById("machineGifWin").style.zIndex  = "1";
   } else {
-    document.getElementsByTagName("humanGifInvalid").style.display = flex;
-    document.getElementsByTagName("MachineGifInvalid").style.display = flex;
+    document.getElementById("humanGifTie").style.display = "flex";
+    document.getElementById("humanGifTie").style.zIndex = "1";
+    document.getElementById("machineGifTie").style.display = "flex";
+    document.getElementById("machineGifTie").style.zIndex  = "1";
   }
 }
 
-rockPaperScissors();
+
+rock.addEventListener("click", rockPaperScissors);
+paper.addEventListener("click", rockPaperScissors);
+scissors.addEventListener("click", rockPaperScissors);
 
 
 
